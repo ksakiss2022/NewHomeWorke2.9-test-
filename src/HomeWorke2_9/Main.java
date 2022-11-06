f5a53c5eddff946eebd64a05bf05573cbd070101
Swetlana <sweta_zaika@mail.ru>
2022-11-07 02:19:50 +0300

    д.з 2.9 на проверку

--- src/HomeWorke2_9/Main.java
+++ src/HomeWorke2_9/Main.java
@@ -110,9 +110,6 @@ public class Main {
 
         System.out.println(createSet());
 
-       // Set<Recipe> recipesBook = new HashSet<>();
-        //recipesBook.add((Recipe) borsh);
-        //recipesBook.add((Recipe) sup);
-        //recipesBook.add(svekolnik);
+
     }
 }
---
+++ src/HomeWorke2_9/NameVerification.java
@@ -0,0 +1,11 @@
+package HomeWorke2_9;
+
+public class NameVerification extends Exception {
+    public NameVerification() {
+        super();
+    }
+
+    public NameVerification(String massage) {
+        super(massage);
+    }
+}
