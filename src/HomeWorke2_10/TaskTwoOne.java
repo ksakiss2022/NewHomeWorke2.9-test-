95e55c2a9353dc624c7a1602fcad7d1b295b86d4
Swetlana <sweta_zaika@mail.ru>
2022-11-11 21:45:46 +0300

    д.з 2.10 попытка 1

--- src/HomeWorke2_10/TaskTwoOne.java
+++ src/HomeWorke2_10/TaskTwoOne.java
@@ -22,10 +22,95 @@ public class TaskTwoOne {
        var listThree= new java.util.ArrayList<>(List.of(2,4,6,8,10));
        for (int i=0; i< 15; i++){
            listThree.add(random.nextInt(10));
+       } var listfour= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listfour.add(random.nextInt( 10));
        }
-       mapFrom.put("Первый список 1:", listOne);
-       mapFrom.put("Втотрой список список 2:", listTwo);
-       mapFrom.put("Третий список список 3:", listThree);
+       var listfive= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listfive.add(random.nextInt(10));
+       }
+       var listSix= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listSix.add(random.nextInt( 10));
+    }
+    var listSeven= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listSeven.add(random.nextInt(10));
+    }
+       var listEight= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listEight.add(random.nextInt(10));
+       }
+       var listNine= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listNine.add(random.nextInt(10));
+       }
+       var listTen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listTen.add(random.nextInt(10));
+       }
+       var listEleven= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listEleven.add(random.nextInt(10));
+       }
+       var listTwelve= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listTwelve.add(random.nextInt(10));
+       }
+       var listThirteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listThirteen.add(random.nextInt(10));
+       }
+       var listFourteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listFourteen.add(random.nextInt(10));
+       }
+       var listFifteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listFifteen.add(random.nextInt(10));
+       }
+       var listSixteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listSixteen.add(random.nextInt(10));
+       }
+       var listSeventeen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listSeventeen.add(random.nextInt(10));
+       }
+       var listEighteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listEighteen.add(random.nextInt(10));
+       }
+       var listNineteen= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listNineteen.add(random.nextInt(10));
+       }
+       var listTwenty= new java.util.ArrayList<>(List.of(2,4,6,8,10));
+       for (int i=0; i< 15; i++){
+           listSeventeen.add(random.nextInt(10));
+       }
+       mapFrom.put("1:", listOne);
+       mapFrom.put("2:", listTwo);
+       mapFrom.put("3:", listThree);
+       mapFrom.put("4:", listfour);
+       mapFrom.put("5:", listfive);
+       mapFrom.put("6:", listSix);
+       mapFrom.put("7:", listSeven);
+       mapFrom.put("8:", listEight);
+       mapFrom.put("9:", listNine);
+       mapFrom.put("10:", listTen);
+       mapFrom.put("11:", listEleven);
+       mapFrom.put("12:", listTwelve);
+       mapFrom.put("13", listThirteen);
+       mapFrom.put("14:", listFourteen);
+       mapFrom.put("15:", listFifteen);
+       mapFrom.put("16:", listSixteen);
+       mapFrom.put("17:", listSeventeen);
+       mapFrom.put("18:", listEighteen);
+       mapFrom.put("19:", listNineteen);
+       mapFrom.put("20:", listTwenty);
+
        System.out.println(mapFrom);
 
        for (var keySet:mapFrom.keySet()){
